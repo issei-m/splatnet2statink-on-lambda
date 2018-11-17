a21f587f99750fea390a64e7c09a35c15e7dcfc5
Issei.M <issei.m7@gmail.com>
2018-11-17 17:41:38 +0900

    Add the AWS Lambda handler for running splatnet2statink

---
+++ handler.py
@@ -0,0 +1,12 @@
+import subprocess
+import re
+
+
+def main(event, context):
+    result = subprocess.run(["./splatnet2statink.py", "-r"], encoding="utf-8", stdout=subprocess.PIPE)
+    formatted_result = re.sub(r"^splatnet2statink .+\nChecking if there are previously-unuploaded battles...\n", "", result.stdout).strip()
+    print(formatted_result)
+
+
+if __name__ == "__main__":
+    main("", "")
